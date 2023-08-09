0e3bde89fd63b0696cbabaccb109490e414c687a
S Santoshi <100473528+S-Santoshi@users.noreply.github.com>
2023-08-09 15:07:44 +0530

    Update app.py

--- TODO/app.py
+++ TODO/app.py
@@ -1,30 +1,58 @@
 from flask import Flask,jsonify,Response,request
 import pandas as pd
-import json
+import os
 
+# future work
 #custom error message with code
 #documentation
 #testcases
 #filtering
 #pagination
 
+#If file does not exist it is created along with a empty dataframe 
+if not os.path.isfile("todo.json"):
+  with open("todo.json", 'w') as f:
+    df=pd.DataFrame(columns=["ID","Title","Status"])
+    df.to_json(f, orient='columns', indent=4)
+ 
+#home route  
 app=Flask(__name__)
 @app.route("/")
 def home():
   return "Hello world"
 
+'''General working of get:
+  read the data from json file convert into dataframe
+  if id given get the record of that id
+  return the dataframe as json object
+
+working of post/put:
+  read the data from json file convert into dataframe
+  if id given get the record of that id
+  update the record (add new, update)
+  write back into the json File
+  return the changed/added record as json
+  
+workign of delete:
+  read the data from json file convert into dataframe
+  get the record of given id
+  drop that record
+  write the changed dataframe into json file
+  '''
+  
+
+#endpoint /todos accepts two methods get and post
 @app.route("/todos",methods=['GET','POST'])
 def todos():
-  global df
   if request.method=="POST":
-    d=request.get_json()
+    d=request.get_json() 
     with open("todo.json", 'r') as f:
-        df = pd.read_json(f)
-    if d['ID'] not in list(df['ID']):
-      todo={"ID":d['ID'],"Title":d['Title'],"Status":d['Status'] }
+        df = pd.read_json(f) 
+    if d['ID'] not in list(df['ID']): # only unique ids are accepted
+      todo={"ID":d['ID'],"Title":d['Title'],"Status":bool(d['Status']) }
       df=df._append(todo,ignore_index=True) 
-      with open("todo.json", 'w') as f:
-        df.to_json(f, orient='records', indent=4)
+      with open("todo.json", 'w') as f: 
+        df.to_json(f, orient='records', indent=4) 
       return jsonify(todo)
     return "Not added. ID already exists"
   elif request.method=="GET":
@@ -33,11 +61,12 @@ def todos():
     data=df.to_dict(orient ='records')
     return jsonify(data)
     
+# endpoint /todos/<id> accepts three methods get, put, delete   
 @app.route("/todos/<id>",methods=['GET','PUT','DELETE'])
 def todo(id):
   with open("todo.json", 'r') as f:
     df = pd.read_json(f)
-  if int(id) in list(df['ID']):
+  if int(id) in list(df['ID']): # assess the methods only if such an id exist
     if request.method=="GET":
       d=df.loc[df['ID']==int(id)]
       print(d)
@@ -49,7 +78,7 @@ def todo(id):
       i=df.index[df['ID']==int(id)]
       for k,v in data.items():
         if k=='ID':
-          if v in list(df['ID']):
+          if v in list(df['ID']): # Existing ID can be changed only if the new mentioned ID doesnt alreday exist
             return jsonify("Cannot be added. New ID already exists")
         df.loc[df['ID']==int(id),k]=v
       d=df.loc[i]
